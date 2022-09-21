c54497ba17e90877cbc9d58de713a2bb556be745
useonguk <102145499+useonguk@users.noreply.github.com>
2022-09-21 11:29:38 +0900

    Add files via upload

---
+++ module/practice.py
@@ -0,0 +1,11 @@
+from theater_module import *
+price(3)
+price_morning(4)
+price_soldier(5)
+
+#other code
+
+# import theater_module
+# theater_module.price(3)
+# theater_module.price_morning(4)
+# theater_module.price_soldier(5)
